a3c50e64aa68344a963d3e35041ed76ed96d5149
albertokalim <alberto.santander@um.es>
2026-03-06 11:51:37 +0100

    Revert "Commit por si acaso"
    This reverts commit 257700254b80ab05be27b3b42fb795dc779b067f.

--- Source/PreAmp.h
+++ Source/PreAmp.h
@@ -33,8 +33,12 @@ namespace preamp {
             return x / (std::abs(x) + 1.f);
         }
 
+        static float asymptoticLimit2(float x) {
+            return (5.f*x) / ((std::abs(5.f*x) + 1.f));
+        }
+
         static float tanh(float x) {
-            return std::tanh(5.f * x);
+            return std::tanh(5.f*x);
         }
 
         static float tanh2(float x) {
