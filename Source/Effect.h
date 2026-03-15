64ee665c72869bb5a98730f8fd097050dbe17095
albertokalim <alberto.santander@um.es>
2026-03-15 12:02:08 +0100

    namespace effects

--- Source/Effect.h
+++ Source/Effect.h
@@ -11,15 +11,20 @@
 #pragma once
 #include <JuceHeader.h>
 #include "Parameters.h"
+namespace effects {
+    class FX {
+    public:
+        virtual void prepare(juce::dsp::ProcessSpec& spec) = 0;
+        virtual void update(parameters::FXParameters& parameters) = 0;
+        virtual void process(juce::dsp::ProcessContextReplacing<float>& context) = 0;
+        bool isBypassed() const { return bypass; }
+        void setBypassed(bool _bypass) { bypass = _bypass; }
 
-class FX {
-public:
-    virtual void prepare(juce::dsp::ProcessSpec& spec) = 0;
-    virtual void update(parameters::FXParameters& parameters) = 0;
-    virtual void process(juce::dsp::ProcessContextReplacing<float>& context) = 0;
-    bool isBypassed() const { return bypass; }
-    void setBypassed(bool _bypass) { bypass = _bypass; }
+    private:
+        bool bypass = true;
+    };
 
-private:
-    bool bypass = true;
-};
+    class DelayFX : public FX {
+
+    };
+}
